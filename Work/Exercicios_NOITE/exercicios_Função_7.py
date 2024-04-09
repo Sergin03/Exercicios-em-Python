e318d7ba712044e6d629ae6820fd184684e3055d
Sergio Henrique <114114763+Sergin03@users.noreply.github.com>
2024-04-09 18:46:38 -0300

    Update exercicios_Função_7.py

--- Work/Exercicios_NOITE/exercicios_Função_7.py
+++ Work/Exercicios_NOITE/exercicios_Função_7.py
@@ -6,3 +6,31 @@
  #O cálculo do valor a ser pago é feito da seguinte forma. Para pagamentos sem atraso, cobrar o valor da prestação. 
 #Quando houver atraso, cobrar 3% de multa, mais 0,1% de juros por dia de atraso:
 
+def valorPagamento(valor, dias_atraso):
+    if dias_atraso == 0:
+        return valor
+    else:
+        multa = valor * 0.03
+        juros = valor * (0.001 * dias_atraso)
+        valor_total = valor + multa + juros
+        return valor_total
+
+relatorio = {'quantidade_prestacoes': 0, 'valor_total': 0}
+
+while True:
+    valor_prestacao = float(input("Digite o valor da prestação (ou 0 para sair): "))
+    if valor_prestacao == 0:
+        break
+    
+    dias_atraso = int(input("Digite o número de dias em atraso: "))
+    
+    valor_a_pagar = valorPagamento(valor_prestacao, dias_atraso)
+    
+    print("Valor a ser pago: R$", valor_a_pagar)
+    
+    relatorio['quantidade_prestacoes'] += 1
+    relatorio['valor_total'] += valor_a_pagar
+
+print("\nRelatório do dia:")
+print("Quantidade de prestações pagas:", relatorio['quantidade_prestacoes'])
+print("Valor total recebido:", relatorio['valor_total'])
